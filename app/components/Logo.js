00cd3698d80e886ffa7ed1e74cb62b672f54d100
Dhruv Chauhan <dhruvchauhan@Dhruvs-MacBook-Air.local>
2025-06-10 23:12:12 +0530

    Added simple client component for testing

--- app/components/Logo.js
+++ app/components/Logo.js
@@ -1,9 +1,9 @@
 function Logo() {
   return (
     <a href="/" className="flex items-center gap-4 z-10">
-      <img src="/logo.png" height="60" width="60" alt="The Wild Oasis logo" />
+      <img src="/logo.png" height="60" width="60" alt="StayCraft logo" />
       <span className="text-xl font-semibold text-primary-100">
-        The Wild Oasis
+        StayCraft
       </span>
     </a>
   );
