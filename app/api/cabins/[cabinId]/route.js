4a417d53fff5a729b122e4b9acd885e44358ccfa
Dhruv Chauhan <chauhandhruv245@gmail.com>
2025-06-23 23:30:24 +0530

    api end point added

---
+++ app/api/cabins/[cabinId]/route.js
@@ -0,0 +1,11 @@
+import { getBookedDatesByCabinId, getCabin } from "@/app/_lib/data-service";
+
+export async function GET(request,{params}){
+    const {cabinId} = params;
+    try{
+        const [cabin,bookingDates] = await Promise.all([getCabin(cabinId),getBookedDatesByCabinId(cabinId)]);
+        return Response.json({cabin,bookingDates});
+    }catch{
+        return Response.json({message:'Cabin could not be found!'})
+    }
+}
